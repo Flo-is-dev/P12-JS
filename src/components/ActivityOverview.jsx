1314d0bd7de1235201c0cd181d8d3dd6855ccdd3
Flo-le-dev <f.gironde.dev@gmail.com>
2024-06-22 02:53:40 +0200

    mock

--- src/components/ActivityOverview.jsx
+++ src/components/ActivityOverview.jsx
@@ -2,33 +2,39 @@ import activity1 from "../assets/img/calories-icon.png"
 import activity2 from "../assets/img/protein-icon.png"
 import activity3 from "../assets/img/carbs-icon.png"
 import activity4 from "../assets/img/fat-icon.png"
+import data from "../data.json";
 
 
 const ActivityOverview = () => {
+
+
+    let keyData = data.data.id12.keyData
+    console.log(keyData);
+
   return (
     <div className="activityContainer">
 
         <div className="activityCard">
             <img src={activity1} alt="icon calories" />
-            <span className="activityNumber">1,930kCal</span>
+            <span className="activityNumber">{keyData.calorieCount}kCal</span>
             <span className="activityTitle">Calories</span>  
         </div>
 
         <div className="activityCard">
             <img src={activity2} alt="icon protéines" />
-            <span className="activityNumber">1,930kCal</span>
+            <span className="activityNumber">{keyData.proteinCount}g</span>
             <span className="activityTitle">Calories</span>  
         </div>
 
         <div className="activityCard">
             <img src={activity3} alt="icon glucides" />
-            <span className="activityNumber">1,930kCal</span>
+            <span className="activityNumber">{keyData.carbohydrateCount}g</span>
             <span className="activityTitle">Calories</span>  
         </div>
 
         <div className="activityCard">
             <img src={activity4} alt="icon lipides" />
-            <span className="activityNumber">1,930kCal</span>
+            <span className="activityNumber">{keyData.lipidCount}g</span>
             <span className="activityTitle">Calories</span>  
         </div>
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,13 @@
 import ActivityOverview from "./ActivityOverview"
+import data from "../data.json";
+
 
 const Content = () => {
+
+//   let data = require("../data/data.json");
+ let keyData = data.data.id12.keyData
+  console.log(keyData);
+
   return (
     <div className="contentBoard">
         <div className="chart"></div>
