a63f4892dff6ecfe473ea19048db6959eeb797bc
Flo-le-dev <f.gironde.dev@gmail.com>
2024-07-10 22:13:09 +0200

    user prform chart P1

--- src/components/DailyActivity.jsx
+++ src/components/DailyActivity.jsx
@@ -10,20 +10,24 @@ const DailyActivity = () => {
     const { userId } = useParams(); 
     const [userDailyActivity, setUserDailyActivity] = useState("")
 
-    // console.log("Activity quotidien",userDailyActivity);
+    console.log("Activity quotidien",userDailyActivity);
 
     const data = [
-        { day: '1', weight: 70, calories: 240 },
-        { day: '2', weight: 69, calories: 220 },
-        { day: '3', weight: 68, calories: 356 },
-        { day: '4', weight: 70, calories: 300 },
-        { day: '5', weight: 69, calories: 220 },
-        { day: '6', weight: 70, calories: 200 },
-        { day: '7', weight: 69, calories: 290 },
-        { day: '8', weight: 70, calories: 350 },
-        { day: '9', weight: 69, calories: 220 },
-        { day: '10', weight: 70, calories: 320 },
-      ];
+        { day: '1', poids: 70, calories: 240 },
+        { day: '2', poids: 69, calories: 220 },
+        { day: '3', poids: 68, calories: 356 },
+        { day: '4', poids: 70, calories: 300 },
+        { day: '5', poids: 69, calories: 220 },
+        { day: '6', poids: 70, calories: 200 },
+        { day: '7', poids: 69, calories: 290 },
+        { day: '8', poids: 70, calories: 350 },
+        { day: '9', poids: 69, calories: 220 },
+        { day: '10', poids: 70, calories: 320 },
+    ];
+
+    //   const dataWeightMin = Math.min(...data.map(item => item.weight));
+    //   const dataWeightMax = Math.max(...data.map(item => item.weight));
+
 
     useEffect(() => {
         const loadData = async () => {
@@ -42,17 +46,18 @@ const DailyActivity = () => {
     <div className="chart"> 
          <ResponsiveContainer width="100%" height="100%">
       <BarChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="day" />
-        <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
-        <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+        <CartesianGrid strokeDasharray="1 1" vertical={false} horizontalPoints={[50,150,400]} />
+        <XAxis dataKey="day" tickLine={false} />
+        <YAxis yAxisId="left" orientation="left" stroke="white" tick={false} />
+        <YAxis yAxisId="right" orientation="right" axisLine={false} tickLine={false} stroke="#74798c" tick={true} ticks={[50,200,400]} domain={[50,400]} />
         <Tooltip  />
-        <Legend verticalAlign="top" height={36} />
-        <Bar yAxisId="left" dataKey="weight" fill="#000000" barSize={20} />
-        <Bar yAxisId="right" dataKey="calories" fill="#FF0000" barSize={20} />
+        <Legend verticalAlign="top" height={36} iconType="circle" iconSize={8} />
+        <Bar yAxisId="left" dataKey="poids" fill="#000000" barSize={10} radius={[10, 10, 0, 0]} />
+        <Bar yAxisId="right" dataKey="calories" fill="#FF0000" barSize={10} radius={[10, 10, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
     </div>
   )
 }
+
 export default DailyActivity
