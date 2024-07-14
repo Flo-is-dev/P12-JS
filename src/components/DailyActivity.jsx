d59bbffe464c3236db21002b580409fa4965c8be
Flo-le-dev <f.gironde.dev@gmail.com>
2024-07-15 00:13:08 +0200

    dailyactivity updated 2.0

--- src/components/DailyActivity.jsx
+++ src/components/DailyActivity.jsx
@@ -44,13 +44,13 @@ const DailyActivity = () => {
   return (
     <div className="chart"> 
          <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={userDailyActivity} margin={{ top: 10, right: 30, left: 0, bottom: 5 }}>
+            <BarChart data={userDailyActivity} barGap={10} margin={{ top: 10, right: 30, left: 0, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="1 1" vertical={false} horizontalPoints={[50,150,400]} />
                 <XAxis dataKey="day" tickLine={false} />
                 <YAxis yAxisId="left" orientation="left" stroke="white" tick={false} />
                 <YAxis yAxisId="right" orientation="right" axisLine={false} tickLine={false} stroke="#74798c" tick={true} ticks={[50,200,400]} domain={[50,400]} />
                 <Tooltip  />
-                <Legend verticalAlign="top" height={36} iconType="circle" iconSize={8} />
+                <Legend verticalAlign="top" height={36} iconType="circle" iconSize={8}  />
                 <Bar yAxisId="left" dataKey="poids" fill="#282D30" barSize={8} radius={[10, 10, 0, 0]} />
                 <Bar yAxisId="right" dataKey="calories" fill="#FF0000" barSize={8} radius={[10, 10, 0, 0]} />
             </BarChart>
